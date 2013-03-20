3b4bcf69dbd810be376d68b56ea2a84e497e5bf9
fcj <fabi@RandomBinaryThoughts.com>
2013-03-20 15:56:12 +0100

    app/AppKernel.php : $this passed to JMSSerializerBundle, why? I can't recall.
    Conflicts:
    	app/AppKernel.php

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,7 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
 
-            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle($this),
             new FOS\RestBundle\FOSRestBundle(),
 
             new APY\DataGridBundle\APYDataGridBundle(),
