5b708c2e3f345b2f8d60c396b36d4800fa08b900
fcj <fabi@RandomBinaryThoughts.com>
2013-04-01 14:26:55 +0200

    APYDataGridBundle : composer.json (dev-master) & AppKernel registration. (cherry picked from commit 4331917a5e1b0e0035873d2387f2f51a403ab1c8)
    Conflicts:
    	app/AppKernel.php
    	composer.json
    
    Conflicts:
    	app/AppKernel.php

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,8 @@ class AppKernel extends Kernel
 
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new FOS\RestBundle\FOSRestBundle(),
+
+            new APY\DataGridBundle\APYDataGridBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
