a81cfd95040343a3e41d67077564e29b6df826f7
Fabien Cadet <cadet@pc126.cines.fr>
2013-07-02 10:35:33 +0200

    Added MopaBootstrapBundle (2.2.x@dev) (NO SASS)
    @see https://github.com/phiamo/MopaBootstrapBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -31,6 +31,8 @@ class AppKernel extends Kernel
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new Craue\FormFlowBundle\CraueFormFlowBundle(),
+
+            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
