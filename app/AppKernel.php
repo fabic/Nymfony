fd18dfda499fa284f6d3f401c6b007613ef320e7
Fabien Cadet <cadet@pc126.cines.fr>
2013-07-02 10:32:05 +0200

    Added CraueFormFlowBundle (dev-master) : composer.json & AppKernel
    @see https://github.com/craue/CraueFormFlowBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -30,6 +30,7 @@ class AppKernel extends Kernel
             new SimpleThings\EntityAudit\SimpleThingsEntityAuditBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new Craue\FormFlowBundle\CraueFormFlowBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
