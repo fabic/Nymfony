2a799fdeb9728124ae7493074e729106d9500f92
fcj <fabi@RandomBinaryThoughts.com>
2013-09-06 20:07:29 +0200

    Upgrading from Symfony 2.2.x to 2.3.y (#1)
    @see https://github.com/symfony/symfony-standard/blob/2.3/UPGRADE-2.3.md
    
    @see also :
    
    	http://symfony.com/blog/symfony-2-3-0-the-first-lts-is-now-available
    	http://symfony.com/blog/new-in-symfony-2-3-what-else
    	http://symfony.com/blog/symfony-2-3-1-released
    	http://symfony.com/blog/symfony-2-3-2-released
    
    @see and when you can spare some time:
    
    https://github.com/symfony/symfony/blob/2.3/UPGRADE-2.3.md

--- app/autoload.php
+++ app/autoload.php
@@ -4,10 +4,12 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 
 $loader = require __DIR__.'/../vendor/autoload.php';
 
+/* fcj.2013-09-06 : Removed, upgrading to v2.3
 // intl
 if (!function_exists('intl_get_error_code')) {
     require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
 }
+*/
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
